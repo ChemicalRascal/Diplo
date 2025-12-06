62d6e079ca5de446151f6ca7724af218c4b3caa5
James Denholm <nod.helm@gmail.com>
2025-12-07 00:57:42 +1100

    Fix routing for Game page

--- src/BSBackupSystem/Areas/Game/Pages/Game.cshtml.cs
+++ src/BSBackupSystem/Areas/Game/Pages/Game.cshtml.cs
@@ -1,10 +1,25 @@
+using BSBackupSystem.Data;
+using BSBackupSystem.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BSBackupSystem.Areas.Game.Pages;
 
-public class GameModel : PageModel
+public class GameModel(AppDbContext appDb) : PageModel
 {
-    public void OnGet(Guid id)
+    [TempData]
+    public string StatusMessage { get; set; } = string.Empty;
+
+    public async Task<IActionResult> OnGet(Guid id)
     {
+        var game = await appDb.Games.FirstOrDefaultAsync(g =>  g.Id == id);
+        if (game is null)
+        {
+            StatusMessage = "Error: No Such Game.";
+            return RedirectToPage("Index");
+        }
+
+        throw new NotImplementedException();
     }
 }
